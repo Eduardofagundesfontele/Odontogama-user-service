5a2702dff6a80b84414b391dffc624c4aea088e7
Eduardo Fagundes <eduardofagundes3@gmail.com>
2026-09-16 17:28:24 -0300

    first commit

---
+++ src/test/java/com/odontogama/odontogama_auth_service/OdontogamaAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odontogama.odontogama_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontogamaAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
